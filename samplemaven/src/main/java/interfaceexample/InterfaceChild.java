2dbd5461da9bd9493895167187abd801d520c0b1
krishnanunni96 <krishnanunni.s.1996@gmail.com>
2025-12-01 07:34:33 +0530

    interface reference

--- samplemaven/src/main/java/interfaceexample/InterfaceChild.java
+++ samplemaven/src/main/java/interfaceexample/InterfaceChild.java
@@ -4,9 +4,13 @@ public class InterfaceChild implements InterfaceParent {
 	int sum = 0;
 
 	public static void main(String[] args) {
-		InterfaceChild obj = new InterfaceChild();
-		obj.get(10, 20);
-		obj.display();
+//		InterfaceChild obj = new InterfaceChild();
+//		obj.get(10, 20);
+//		obj.display();
+
+		InterfaceParent obj2 = new InterfaceChild();
+		obj2.get(10, 20);
+		obj2.display();
 	}
 
 	@Override
